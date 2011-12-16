f25cbeb2b2c307176a78bd30dd37fdc3f8a9abdd
Mike Heffner <mikeh@fesnel.com>
2011-12-16 01:51:14 -0500

    Remove UTF-8 characters from code.

--- lib/scamp/rooms.rb
+++ lib/scamp/rooms.rb
@@ -2,7 +2,7 @@ class Scamp
   module Rooms
     # TextMessage (regular chat message),
     # PasteMessage (pre-formatted message, rendered in a fixed-width font),
-    # SoundMessage (plays a sound as determined by the message, which can be either “rimshot”, “crickets”, or “trombone”),
+    # SoundMessage (plays a sound as determined by the message, which can be either "rimshot", "crickets", or "trombone"),
     # TweetMessage (a Twitter status URL to be fetched and inserted into the chat)
 
     def upload
